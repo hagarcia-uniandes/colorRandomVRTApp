a5110fe71fba7ab85d47e9e758eb9bf318c21e5b
Holman Garcia <ha.garcia@uniandes.edu.co>
2020-03-14 18:51:48 -0500

    Prueba

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# colorRandomVRTApp
+# MISO-4208-visualTesting
 
+`heroku run bash --app [appName]`
--- cypress/integration/randomColorTest.js
+++ cypress/integration/randomColorTest.js
@@ -2,15 +2,15 @@ describe('Random color palette', () => {
 
   context('Generate Random Colors', () => {
     beforeEach(function () {
-      cy.visit('https://hagarcia-uniandes.github.io/VRT_colorPallete/')
+      cy.visit('https://daprieto1.github.io/MISO-4208-randomColors/')
     })
 
     it('T1: Should generate random color from empty palette', () => {
       cy.contains('Paleta de colores')
       cy.contains(' Generar nueva paleta').click()
-      cy.screenshot('Test1-before')
+      cy.screenshot('T1-before')
       cy.contains(' Generar nueva paleta').click()
-      cy.screenshot('Test1-after')
+      cy.screenshot('T1-after')
     })
 
   })

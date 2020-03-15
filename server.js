2317c3d664762e86128300e7eb1498b3209366ba
Holman Garcia <ha.garcia@uniandes.edu.co>
2020-03-14 20:06:27 -0500

    App

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ 'extended': 'true' }));            // parse appl
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
-console.log('aca');
+
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -38,7 +38,7 @@ app.use(function (req, res, next) {
 });
 console.log('aca');
 var executionRouter = require('./controllers/ExecutionController')(Execution);
-
+console.log('aca');
 app.use('/api/execution', executionRouter);
 
 app.get('*', function (req, res) {

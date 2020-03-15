bfb77c39b8f2bfcd95d5aec039336e59ddaa3afd
Holman Garcia <ha.garcia@uniandes.edu.co>
2020-03-14 19:56:22 -0500

    App

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ mongoose.connect(mongoUri, function (err, res) {
 })
     .catch(err => console.log(err));
 
+console.log('aca');
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({ 'extended': 'true' }));            // parse application/x-www-form-urlencoded
